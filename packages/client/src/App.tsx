709ef027f1391a4b13f419435ce22550664b6fb8
Yi Zu <zuyi@bu.edu>
2021-02-02 11:37:54 +0100

    Setup ant design

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 function App() {
 
@@ -13,20 +15,7 @@ function App() {
   return (
     <ApolloProvider client={client} >
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Title>Interactive Pokédex</Title>
       </div>
     </ApolloProvider>
   );
