56aea4cc47d041e616cf78462a3021344a1c62e6
Yi Zu <zuyi@bu.edu>
2021-02-03 13:16:57 +0100

    Refactored PokeTable to take props from parent component

--- packages/client/src/components/PokeTable.tsx
+++ packages/client/src/components/PokeTable.tsx
@@ -1,78 +1,52 @@
 import React from 'react';
-import { gql} from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+import { ApolloError } from 'apollo-boost';
 import { Table } from 'antd';
 
-type PokemonEdge = {
-    node: Pokemon;
-}
-  
+
 type Pokemon = {
-    id: string;
-    name: string;
-    types: Array<string>;
-    classification: string;
+  id: string;
+  name: string;
+  types: Array<string>;
+  classification: string;
+}
+interface PokeTableProps {
+  pokemons: Array<Pokemon>;
+  loading: boolean;
+  error: ApolloError | undefined;
 }
 
-const getPokemonQuery = gql`
-    {
-      pokemons(q:"${'a'}"){ 
-      edges {
-        node {
-          name
-          types
-          id
-          classification
-        }
-      }
-      pageInfo{
-        hasNextPage
-        endCursor
-      }
-    }}
-    `
-
-function PokeTable() {
-    const { loading, error, data } = useQuery(getPokemonQuery);
-    let result;
-    if (loading === false) {
-        result = data.pokemons.edges.map((edge: PokemonEdge) => {
-        return { key: edge.node.id, name: edge.node.name, types: edge.node.types, classification: edge.node.classification }
-      });
-    }
-
+function PokeTable({pokemons, loading, error}: PokeTableProps) {
 
     const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-{
-      title: 'Type',
-      dataIndex: 'types',
-      key: 'types',
-      render: (types: Array<string>) => (
-        <>
-          {types.map((type: string) => {
-            return type + ' '
-          })}
-        </>
-      ),
-    },
-    {
-      title: 'Classification',
-      dataIndex: 'classification',
-      key: 'classification',
-    },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Type',
+        dataIndex: 'types',
+        key: 'types',
+        render: (types: Array<string>) => (
+          <>
+            {types.map((type: string) => {
+              return type + ' '
+            })}
+          </>
+        ),
+      },
+      {
+        title: 'Classification',
+        dataIndex: 'classification',
+        key: 'classification',
+      },
     ];
- 
     
     if (loading) return <p>Loading...</p>
     if (error) return <p>Something went wrong</p>
 
   return (
-      <Table dataSource={result} columns={columns} pagination={false} />
+      <Table dataSource={pokemons} columns={columns} pagination={false} />
     )
 }
 
