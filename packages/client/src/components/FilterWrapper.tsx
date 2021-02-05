e9674ba8b87342042a1c07521c46c8f5dd540f1d
Yi Zu <zuyi@bu.edu>
2021-02-05 20:40:59 +0100

    Remove unused imports and console.log

--- packages/client/src/components/FilterWrapper.tsx
+++ packages/client/src/components/FilterWrapper.tsx
@@ -1,28 +1,30 @@
-import React, { ChangeEvent, useState } from 'react';
-import { Button, Input, Radio, RadioChangeEvent } from 'antd';
+import React, { useState } from 'react';
+import { Radio, RadioChangeEvent } from 'antd';
 import FilterByName from './FilterByName';
 import FilterByType from './FilterByType';
 
 function FilterWrapper() {
   const [filterType, setFilterType] = useState<string>('byName');
   const onRadioChange = (e: RadioChangeEvent) => {
-    console.log('radio checked', e.target.value);
     setFilterType(e.target.value);
   };
   return (
-    <div>
-      <Radio.Group
-        defaultValue='byName'
-        buttonStyle='solid'
-        onChange={onRadioChange}
-      >
-        <Radio.Button value='byName'>Name</Radio.Button>
-        <Radio.Button value='byType'>Type</Radio.Button>
-      </Radio.Group>
-
-      {filterType === 'byName' && <FilterByName />}
-      {filterType === 'byType' && <FilterByType />}
-    </div>
+    <>
+      <div>
+        <Radio.Group
+          defaultValue='byName'
+          buttonStyle='solid'
+          onChange={onRadioChange}
+        >
+          <Radio.Button value='byName'>Name</Radio.Button>
+          <Radio.Button value='byType'>Type</Radio.Button>
+        </Radio.Group>
+      </div>
+      <div>
+        {filterType === 'byName' && <FilterByName />}
+        {filterType === 'byType' && <FilterByType />}
+      </div>
+    </>
   );
 }
 
