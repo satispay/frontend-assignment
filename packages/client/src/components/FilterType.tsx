957de343c2fad250fa75a08bb28bf01a88edf82b
Yi Zu <zuyi@bu.edu>
2021-02-05 11:48:21 +0100

    Switched to Select component for dropdown

--- packages/client/src/components/FilterType.tsx
+++ packages/client/src/components/FilterType.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import { Menu, Dropdown } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Select } from 'antd';
 import { pokeTypes } from '../constants/index';
 
-function FilterType() {
-  const menu = (
-    <Menu>
-      {pokeTypes.map((pokeType) => (
-        <Menu.Item>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='http://www.alipay.com/'
-          >
-            {pokeType}
-          </a>
-        </Menu.Item>
-      ))}
-    </Menu>
-  );
+const { Option } = Select;
 
+function handleChange(value: any) {
+  console.log(`selected ${value}`);
+}
+
+function FilterType() {
   return (
     <>
-      <Dropdown overlay={menu}>
-        <a className='ant-dropdown-link' onClick={(e) => e.preventDefault()}>
-          Filter by Type <DownOutlined />
-        </a>
-      </Dropdown>
+      <Select defaultValue='All' style={{ width: 120 }} onChange={handleChange}>
+        {pokeTypes.map((pokeType) => (
+          <Option value={pokeType}>{pokeType}</Option>
+        ))}
+      </Select>
     </>
   );
 }
