7b4caa63e9fcb5a81ea3710d7def82e357aab952
Yi Zu <zuyi@bu.edu>
2021-02-05 12:45:49 +0100

    Display list of pokemon name based on selected Type on dropdown

--- packages/client/src/components/FilterType.tsx
+++ packages/client/src/components/FilterType.tsx
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select } from 'antd';
 import { pokeTypes } from '../constants/index';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 const { Option } = Select;
 
-function handleChange(value: any) {
-  console.log(`selected ${value}`);
-}
+type PokemonEdge = {
+  node: Pokemon;
+};
+
+type Pokemon = {
+  id: string;
+  name: string;
+  types: Array<string>;
+  classification: string;
+};
+
+const FILTER_TYPE_QUERY = gql`
+  query FilterTypeQuery($type: String!, $after: ID) {
+    pokemonsByType(type: $type, after: $after) {
+      edges {
+        node {
+          name
+          types
+          id
+          classification
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
 
 function FilterType() {
+  const [typeFilter, setTypeFilter] = useState<string>('Normal');
+
+  const { data } = useQuery(FILTER_TYPE_QUERY, {
+    variables: { type: typeFilter, after: '000' },
+  });
+
+  // const [executeSearch, { data }] = useLazyQuery(FILTER_TYPE_QUERY);
+
+  function handleTypeSelect(event: string) {
+    console.log(`selected ${event}`);
+    setTypeFilter(event);
+  }
+
   return (
     <>
-      <Select defaultValue='All' style={{ width: 120 }} onChange={handleChange}>
+      <Select
+        defaultValue='Normal'
+        style={{ width: 120 }}
+        onChange={handleTypeSelect}
+      >
         {pokeTypes.map((pokeType) => (
-          <Option value={pokeType}>{pokeType}</Option>
+          <Option
+            key={pokeType}
+            value={pokeType}
+            // onClick={() =>
+            //   executeSearch({
+            //     variables: { type: typeFilter },
+            //   })
+            // }
+          >
+            {pokeType}
+          </Option>
         ))}
       </Select>
+      {data &&
+        data.pokemonsByType.edges.map((edge: PokemonEdge) => (
+          <li key={edge.node.id}>{edge.node.name}</li>
+        ))}
     </>
   );
 }
